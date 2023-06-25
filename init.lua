48f0fce7e1c11bb0a8b1f3971cab234a7c244e08
juan <lealjuanm@gmail.com>
2023-06-25 19:04:03 -0400

    new setting

--- init.lua
+++ init.lua
@@ -1,13 +1,1 @@
-require("lealjuanm.config")
-require("lealjuanm.packer")
-require("lealjuanm.remap")
-print("hi Juan")
-
--- https://github.com/ThePrimeagen/init.lua/tree/master
--- https://github.com/josean-dev/dev-environment-files/tree/main/.config/nvim
-
-function sheet()
- print("https://vim.rtorr.com/")
-end
-
-
+require("lealjuanm")
